5caa13c54ae3cd71fc23db5dea0120a9ba87b108
Akash Singh <Akash Singh>
2016-01-13 00:16:10 +0530

    sieve of erastothenes completed

--- EratosthenesSieve.py
+++ EratosthenesSieve.py
@@ -1,3 +1,15 @@
 upperLimit = 1000
+primeList = []
+for i in range(2, upperLimit):
+    primeList.append(i)
 
-numArray = [][1000]
+for i in primeList:
+    if i != "x":
+        j = primeList.index(i) + i
+        while j < (upperLimit - 2):
+            primeList[j] = "x"
+            j += i
+
+for i in primeList:
+    if i != "x":
+        print(i, end=", ")
